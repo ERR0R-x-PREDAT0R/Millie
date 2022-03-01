0eed27f7e39209f9e249785854850dd24cffb060
ALINSHAN <92071951+ERR0R-x-PREDAT0R@users.noreply.github.com>
2022-03-01 11:19:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 let fs = require('fs')
-global.owner = ['91938391460'] // Put your number here
+global.owner = ['919383491460'] // Put your number here
 global.mods =  JSON.parse(fs.readFileSync('./src/moderator.json'))// Moderator?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Premium users do not need a limit
 global.tod = JSON.parse(fs.readFileSync('./src/tod.json'))
